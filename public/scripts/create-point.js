8210a6b335dafd2e206474a104e7a4e164ea14c5
William <William.frazao91@gmail.com>
2020-06-11 20:02:09 -0300

    a milha extra

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -62,6 +62,8 @@ function handleSelectedItem(event) {
 
     const itemId = itemLi.dataset.id
 
+    console.log('ITEM ID: ', itemId)
+
     // verificar se existem itens selecionados, se sim
     // pegar os itens selecionados
 
@@ -86,6 +88,8 @@ function handleSelectedItem(event) {
         // adicionar á seleção
         selectedItems.push(itemId)
     }
+
+    console.log('selectedItems: ', selectedItems)
 
     // atualizar o campo escondido com os itens selecionados
     collectedItems.value = selectedItems
